f4b526d11a66df603d67941921db94271b117aa9
Jakub Šenkýř <senkyr@spstrutnov.cz>
2022-10-11 12:26:51 +0200

    Založení projektu

---
+++ Source/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Malovani
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.WriteLine("ASCII Malování");
+            Console.WriteLine("--------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Pro ukončení stiskni libovolnou klávesu...");
+            Console.CursorVisible = false;
+            Console.ReadKey();
+        }
+    }
+}
