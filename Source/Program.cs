710576ddbaf76d0c60a536ccad2ff5543b32b81c
Jakub_Polák <polakj21@spstrutnov.cz>
2022-10-17 11:31:30 +0200

    psaní na kurzor
    bude vylepšeno (snad)

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Malovani
+﻿using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Transactions;
+
+namespace Malovani
 {
     internal class Program
     {
@@ -13,20 +17,35 @@
             NastaveniBarev();
             Uvod();
 
-            char[,] obrazek = ZiskatObrazek();
+            char[,] obrazek;// = ZiskatObrazek();
             PoziceKurzoru kurzor = new PoziceKurzoru() { X = 0, Y = 0 };
+            char[] Blacklist; //seznam nefunkčních kláves
+            Blacklist = new char[2] { 'f', (char)14};//nebere to \n (14 není \n)
 
-            bool konec;
+            //dočasné rešení
+            obrazek = new char[,] { {'o','o'}, {'o','o'} };
+
+            bool konec = false;
             do
             {
-                VykresleniObrazku(obrazek);
-                VykresleniKurzoru(kurzor);
+                //VykresleniObrazku(obrazek);
+                //VykresleniKurzoru(kurzor); aby to běželo
 
                 ConsoleKeyInfo novaKlavesa = Console.ReadKey();
 
-                VlivOvladaniNaObrazek(novaKlavesa, obrazek);
-                kurzor = VlivOvladaniNaKurzor(novaKlavesa);
-                konec = ZnaciKonec(novaKlavesa);
+                VlivOvladaniNaObrazek(novaKlavesa, obrazek, Blacklist, kurzor);
+                //kurzor = VlivOvladaniNaKurzor(novaKlavesa);
+                //konec = ZnaciKonec(novaKlavesa); rovněž aby ro  to běžlo
+                for (int x = 0; x < obrazek.GetLength(0); x++)
+                {
+                    for(int y = 0; y < obrazek.GetLength(1); y++)
+                    {
+                        Console.Write(obrazek[x,y]);// je to nehezké => nejdřív x, pak y
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+                Console.WriteLine();
             } while (!konec);
 
             UlozeniObrazku(obrazek);
@@ -43,9 +62,22 @@
             throw new NotImplementedException();
         }
 
-        static void VlivOvladaniNaObrazek(ConsoleKeyInfo novaKlavesa, char[,] obrazek)
+        static void VlivOvladaniNaObrazek(ConsoleKeyInfo novaKlavesa, char[,] obrazek, char[] Blacklist, PoziceKurzoru kurzor)
         {
-            throw new NotImplementedException();
+            if (!Blacklist.Contains(novaKlavesa.KeyChar))
+            {
+                Console.Write("something");
+                switch (novaKlavesa.KeyChar)
+                {
+                    case (char)13:
+                        Console.Write(" -- enter -- ");
+                        break;
+                    default:
+                        obrazek[kurzor.X, kurzor.Y] = novaKlavesa.KeyChar;
+                        break;
+                }
+            }
+            Console.WriteLine(" -- "+novaKlavesa.KeyChar);
         }
 
         static PoziceKurzoru VlivOvladaniNaKurzor(ConsoleKeyInfo novaKlavesa)
