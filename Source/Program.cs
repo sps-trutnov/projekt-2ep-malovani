2740c446243dc583cedbc034af8c26dbf2ac3ba8
Martin Michálek <michalekm21@spstrutnov.cz>
2022-10-17 11:28:56 +0200

    Pohyb Kurzoru
    Problémy s Podmínkami Velikosti Obrázku.

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,15 +1,11 @@
-﻿namespace Malovani
-{
-    internal class Program
-    {
-        struct PoziceKurzoru
-        {
+﻿namespace Malovani {
+    internal class Program {
+        struct PoziceKurzoru {
             public int X;
             public int Y;
         }
 
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             NastaveniBarev();
             Uvod();
 
@@ -17,15 +13,14 @@
             PoziceKurzoru kurzor = new PoziceKurzoru() { X = 0, Y = 0 };
 
             bool konec;
-            do
-            {
+            do {
                 VykresleniObrazku(obrazek);
                 VykresleniKurzoru(kurzor);
 
                 ConsoleKeyInfo novaKlavesa = Console.ReadKey();
 
                 VlivOvladaniNaObrazek(novaKlavesa, obrazek);
-                kurzor = VlivOvladaniNaKurzor(novaKlavesa);
+                kurzor = VlivOvladaniNaKurzor(novaKlavesa, kurzor, obrazek);
                 konec = ZnaciKonec(novaKlavesa);
             } while (!konec);
 
@@ -33,50 +28,51 @@
             Rozlouceni();
         }
 
-        static void UlozeniObrazku(char[,] obrazek)
-        {
+        static void UlozeniObrazku(char[,] obrazek) {
             throw new NotImplementedException();
         }
 
-        static bool ZnaciKonec(ConsoleKeyInfo novaKlavesa)
-        {
+        static bool ZnaciKonec(ConsoleKeyInfo novaKlavesa) {
             throw new NotImplementedException();
         }
 
-        static void VlivOvladaniNaObrazek(ConsoleKeyInfo novaKlavesa, char[,] obrazek)
-        {
+        static void VlivOvladaniNaObrazek(ConsoleKeyInfo novaKlavesa, char[,] obrazek) {
             throw new NotImplementedException();
         }
 
-        static PoziceKurzoru VlivOvladaniNaKurzor(ConsoleKeyInfo novaKlavesa)
-        {
+        static PoziceKurzoru VlivOvladaniNaKurzor(ConsoleKeyInfo novaKlavesa, PoziceKurzoru kurzor, char[,] obrazek) {
+            if ((novaKlavesa.Key == ConsoleKey.LeftArrow) && kurzor.X <= obrazek.GetLength(0)) {
+                kurzor.X = kurzor.X + 1;
+            } else if ((novaKlavesa.Key == ConsoleKey.RightArrow) && kurzor.X >= 0) {
+                kurzor.X = kurzor.X - 1;
+            } else if ((novaKlavesa.Key == ConsoleKey.UpArrow) && kurzor.X < obrazek.GetLength(1)) {
+                kurzor.Y = kurzor.Y - 1;
+            } else if ((novaKlavesa.Key == ConsoleKey.DownArrow) && kurzor.X >= 0) {
+                kurzor.Y = kurzor.Y + 1;
+            }
+            
+            return kurzor;
+        }
+
+        static void VykresleniKurzoru(PoziceKurzoru kurzor) {
             throw new NotImplementedException();
         }
 
-        static void VykresleniKurzoru(PoziceKurzoru kurzor)
-        {
+        static void VykresleniObrazku(char[,] obrazek) {
             throw new NotImplementedException();
         }
 
-        static void VykresleniObrazku(char[,] obrazek)
-        {
+        static char[,] ZiskatObrazek() {
             throw new NotImplementedException();
         }
 
-        static char[,] ZiskatObrazek()
-        {
-            throw new NotImplementedException();
-        }
-
-        static void NastaveniBarev()
-        {
+        static void NastaveniBarev() {
             Console.ForegroundColor = ConsoleColor.Black;
             Console.BackgroundColor = ConsoleColor.White;
             Console.Clear();
         }
 
-        static void Uvod()
-        {
+        static void Uvod() {
             Console.Clear();
             Console.WriteLine("ASCII Malování");
             Console.WriteLine("--------------");
@@ -88,8 +84,7 @@
             Console.Clear();
         }
 
-        static void Rozlouceni()
-        {
+        static void Rozlouceni() {
             Console.Clear();
             Console.WriteLine("Děkujeme za použití naší aplikace!");
             Console.WriteLine();
