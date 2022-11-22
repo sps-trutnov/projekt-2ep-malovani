829ced418ff2c271f8342f17ea7cc26bb8146f13
Jakub Šenkýř <senkyr@spstrutnov.cz>
2022-11-22 11:07:52 +0100

    Oprava rozsahu pohybu kurzoru

--- Source/Program.cs
+++ Source/Program.cs
@@ -105,7 +105,7 @@ namespace Malovani
 
         static PoziceKurzoru VlivOvladaniNaKurzor(ConsoleKeyInfo novaKlavesa, PoziceKurzoru kurzor, PoziceKurzoru max)
         {
-            if (novaKlavesa.Key == ConsoleKey.RightArrow && kurzor.X < max.X + 1)
+            if (novaKlavesa.Key == ConsoleKey.RightArrow && kurzor.X < max.X)
             {
                 kurzor.X = kurzor.X + 1;
             }
@@ -117,7 +117,7 @@ namespace Malovani
             {
                 kurzor.Y = kurzor.Y - 1;
             }
-            else if (novaKlavesa.Key == ConsoleKey.DownArrow && kurzor.Y < max.Y - 1)
+            else if (novaKlavesa.Key == ConsoleKey.DownArrow && kurzor.Y < max.Y)
             {
                 kurzor.Y = kurzor.Y + 1;
             }
